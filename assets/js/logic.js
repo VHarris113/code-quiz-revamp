337b0e9d4f9f46865ef420fb50f8535611d46042
Veronica Harris <veronica.n.harris92@gmail.com>
2022-03-31 10:32:04 -0400

    finished the questions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,28 @@
-// Separate scores and logic js
-
-// getting the questions on the page
-
-// trying to get the feedback from answers
+// Questions for the Quiz
+var questions = [
+    {
+        title: "What team does Captain America lead?",
+        choices: ["X-Men", "The Defenders", "Suicide Squad", "Avengers"],
+        answer: "Avengers"
+    },
+    {
+        title: "What is covering Wolverine's skeleton?",
+        choices: ["Adamantium", "Unobtanium", "Vibranium", "Badassium"],
+        answer: "Adamantium"
+    },
+    {
+        title: "Where is Natasha Romanoff (Black Widow) from?",
+        choices: ["England", "India", "Russia", "Japan"],
+        answer: "Russia"
+    },
+    {
+        title: "What is Cyclops' mutant ability?",
+        choices: ["Telekninesis", "Eye Lasers", "Speed (i.e. Running Fast", "Talking to Animals"],
+        answer: "Eye Lasers"
+    },
+    {
+        title: "Who plays Spider-Man on the big screen?",
+        choices: ["Toby Maguire", "Tom Holland", "Andrew Garfield", "All of the Above"],
+        answer: "All of the Above"
+    },
+];
